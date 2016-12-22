294484606cf0ad86f900f70ed05ce34f7b968f50
Philipp Schumann <philippschumann97@gmail.com>
2016-12-22 21:16:57 +0100

    add window configuration

--- desktop/src/com/game/desktop/DesktopLauncher.java
+++ desktop/src/com/game/desktop/DesktopLauncher.java
@@ -7,6 +7,21 @@ import com.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		boolean borderLess = false;
+		config.useGL30 = true;
+		if (borderLess) {
+			config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width ;
+			config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height ;
+		} else {
+			config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width / 2;
+			config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height / 2;
+		}
+		//config.fullscreen=true;
+		config.vSyncEnabled = true;
+		config.resizable = false;
+		//System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+
 		new LwjglApplication(new Game(), config);
 	}
 }
